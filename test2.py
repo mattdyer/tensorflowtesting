d73bc4b8109103d327c3d75a6b24fdb901cf5bcb
mattdyer <madmatt1220@gmail.com>
2017-12-09 15:24:10 -0700

    working through getting started example

--- test2.py
+++ test2.py
@@ -8,11 +8,17 @@ x = tf.placeholder(tf.float32)
 linear_model = W*x + b
 
 init = tf.global_variables_initializer()
-sess.run(init)
+#sess.run(init)
 
 y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
-print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 
-#print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+sess.run(init) # reset values to incorrect defaults.
+for i in range(1000):
+  sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+
+print(sess.run([W, b]))
