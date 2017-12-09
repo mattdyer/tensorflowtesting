f738aafbc4dd86b4087c60df2119b2f5e8fb6a81
mattdyer <madmatt1220@gmail.com>
2017-12-09 15:01:38 -0700

    working through getting started example

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 import tensorflow as tf 
 
-hello = tf.constant('Hello, TensorFlow!')
+node1 = tf.constant(3.0, dtype=tf.float32)
+node2 = tf.constant(4.0) # also tf.float32 implicitly
+
+adder = node1 + node2
+
 sess = tf.Session()
-print(sess.run(hello))
+
+print(node1, node2)
+
+print(sess.run(adder))
+
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b  # + provides a shortcut for tf.add(a, b)
+
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
